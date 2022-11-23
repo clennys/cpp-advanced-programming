fabc0792cd58a88d7a902af469c9368302686a18
Dennys Huber <dennys.huber@pm.me>
2022-11-23 01:11:22 +0100

    update

--- lecture03/ex_05/src/main.cc
+++ lecture03/ex_05/src/main.cc
@@ -5,12 +5,18 @@
 #include "../include/player.h"
 #include "../include/playfield.h"
 
+// Joe Bauer
+#include "../include/computer_player.h"
+
 using namespace std;
 
 int main() {
   AI<playfield> p1;
-  AI<playfield> p2;
-  game<playfield, AI<playfield>, AI<playfield>> game(p2, p1);
+  // AI<playfield> p2;
+  player<playfield> p2;
+  // computer_player<playfield> p2;
+  game<playfield, AI<playfield>, player<playfield>> game(p1, p2);
+  // game<playfield, AI<playfield>, computer_player<playfield>> game(p1, p2);
   game.run();
 
   return 0;
